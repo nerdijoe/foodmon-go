c9d87efc5c1c5038907038900f9c57cf3f378b6a
Bill Ivan Kooslarto <billivank_96@yahoo.co.id>
2017-06-13 15:34:27 +0700

    fixed swiper bug

--- client/foodmongo/src/components/Swiper.js
+++ client/foodmongo/src/components/Swiper.js
@@ -6,53 +6,84 @@ import { Container, Icon, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Lef
 class Swiper extends Component {
 
   handleSwiper() {
-    const restaurants = [...this.props.restaurants];
-    const user = this.props.user.interestArr;
-    const card = [];
-    if (user) {
-      for (let i = 0; i < restaurants.length; i++) {
-        for (let j = 0; j < user.length; j++) {
-          const regex = new RegExp(user[j].cuisine_name, 'gi')
-          if (restaurants[i].restaurant.cuisines.match(regex)) {
-            card.push(restaurants[i]);
-            restaurants.splice(restaurants.indexOf(restaurants[i]), 1);
-            break;
+    if(this.props.user.interestArr){
+      const restaurants = [...this.props.restaurants];
+      const user = this.props.user.interestArr;
+      const card = [];
+      if (user) {
+        for (let i = 0; i < restaurants.length; i++) {
+          for (let j = 0; j < user.length; j++) {
+            const regex = new RegExp(user[j].cuisine_name, 'gi')
+            if (restaurants[i].restaurant.cuisines.match(regex)) {
+              card.push(restaurants[i]);
+              restaurants.splice(restaurants.indexOf(restaurants[i]), 1);
+              break;
+            }
           }
         }
       }
+      const interest = user.map((val) => {
+        return val.cuisine_name;
+      }).join(' ');
+      const cards = card.concat(restaurants);
+      return (
+        <DeckSwiper
+          dataSource={cards}
+          renderItem={item =>
+            (<Card style={{ elevation: 3 }}>
+              <CardItem>
+                <Left>
+                  <Thumbnail source={{ uri: item.restaurant.featured_image }} />
+                  <Body>
+                    <Text>{item.restaurant.name}</Text>
+                    <Text note>{item.restaurant.cuisines}</Text>
+                  </Body>
+                </Left>
+              </CardItem>
+              <CardItem cardBody>
+                <Body style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+                  <Image
+                    style={{ width: 270, height: 220 }}
+                    source={{ uri: item.restaurant.featured_image }}
+                  />
+                  <Text note style={{ margin: 20, textAlign: 'center' }}>{item.restaurant.location.address}</Text>
+                </Body>
+              </CardItem>
+              {this.handleIcon(item.restaurant.cuisines, interest)}
+            </Card>)
+          }
+        />
+      );
+    } else {
+      const restaurants = [...this.props.restaurants];
+      return (
+        <DeckSwiper
+          dataSource={restaurants}
+          renderItem={item =>
+            (<Card style={{ elevation: 3 }}>
+              <CardItem>
+                <Left>
+                  <Thumbnail source={{ uri: item.restaurant.featured_image }} />
+                  <Body>
+                    <Text>{item.restaurant.name}</Text>
+                    <Text note>{item.restaurant.cuisines}</Text>
+                  </Body>
+                </Left>
+              </CardItem>
+              <CardItem cardBody>
+                <Body style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+                  <Image
+                    style={{ width: 270, height: 220 }}
+                    source={{ uri: item.restaurant.featured_image }}
+                  />
+                  <Text note style={{ margin: 20, textAlign: 'center' }}>{item.restaurant.location.address}</Text>
+                </Body>
+              </CardItem>
+            </Card>)
+          }
+        />
+      );
     }
-    const interest = user.map((val) => {
-      return val.cuisine_name;
-    }).join(' ');
-    const cards = card.concat(restaurants);
-    return (
-      <DeckSwiper
-        dataSource={cards}
-        renderItem={item =>
-        <Card style={{ elevation: 3 }}>
-          <CardItem>
-            <Left>
-              <Thumbnail source={{ uri: item.restaurant.featured_image }} />
-              <Body>
-                <Text>{item.restaurant.name}</Text>
-                <Text note>{item.restaurant.cuisines}</Text>
-              </Body>
-            </Left>
-          </CardItem>
-          <CardItem cardBody>
-            <Body style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-              <Image
-                style={{ width: 270, height: 220 }}
-                source={{ uri: item.restaurant.featured_image }}
-              />
-              <Text note style={{ margin: 20, textAlign: 'center' }}>{item.restaurant.location.address}</Text>
-            </Body>
-          </CardItem>
-          {this.handleIcon(item.restaurant.cuisines, interest)}
-        </Card>
-        }
-      />
-    );
   }
 
   handleIcon(cuisines, interest) {
