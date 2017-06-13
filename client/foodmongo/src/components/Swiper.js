544a7989133f10814c7921cf87d0ac3e44facdcd
edimdendidiwangga <edimdendy@gmail.com>
2017-06-13 14:47:51 +0700

    swiper fav

--- client/foodmongo/src/components/Swiper.js
+++ client/foodmongo/src/components/Swiper.js
@@ -6,9 +6,28 @@ import { Container, Icon, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Lef
 class Swiper extends Component {
 
   handleSwiper() {
+    const restaurants = [...this.props.restaurants];
+    const user = this.props.user.interestArr;
+    const card = [];
+    if (user) {
+      for (let i = 0; i < restaurants.length; i++) {
+        for (let j = 0; j < user.length; j++) {
+          const regex = new RegExp(user[j].cuisine_name, 'gi')
+          if (restaurants[i].restaurant.cuisines.match(regex)) {
+            card.push(restaurants[i]);
+            restaurants.splice(restaurants.indexOf(restaurants[i]), 1);
+            break;
+          }
+        }
+      }
+    }
+    const interest = user.map((val) => {
+      return val.cuisine_name;
+    }).join(' ');
+    const cards = card.concat(restaurants);
     return (
       <DeckSwiper
-        dataSource={this.props.restaurants}
+        dataSource={cards}
         renderItem={item =>
         <Card style={{ elevation: 3 }}>
           <CardItem>
@@ -29,14 +48,23 @@ class Swiper extends Component {
               <Text note style={{ margin: 20, textAlign: 'center' }}>{item.restaurant.location.address}</Text>
             </Body>
           </CardItem>
-          <CardItem>
-            <Icon name="heart" style={{ color: '#ED4A6A' }} />
-            <Text>{item.restaurant.name}</Text>
-          </CardItem>
+          {this.handleIcon(item.restaurant.cuisines, interest)}
         </Card>
         }
       />
     );
+  }
+
+  handleIcon(cuisines, interest) {
+    const cuisines2 = cuisines.split(',').map(item => item.trim());
+    if ((new RegExp('\\b' + cuisines2.join('\\b|\\b') + '\\b')).test(interest)) {
+      return (
+        <CardItem>
+          <Icon name="md-star" style={{ color: '#F6C90E' }} />
+          <Text>{cuisines}</Text>
+        </CardItem>
+      );
+    }
   }
 
   render() {
@@ -56,6 +84,7 @@ class Swiper extends Component {
 }
 
 const mapStateToProps = state => ({
+  user: state.UserReducer,
   restaurants: state.restaurants,
 });
 
